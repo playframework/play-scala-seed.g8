44f361e1ca8c006b6ee4b719f435877b18d3d784
Will Sargent <wsargent@users.noreply.github.com>
2016-10-26 17:43:30 -0700

    cleanup (#6)

--- src/main/scaffolds/form/generated-test/controllers/$model__Camel$ControllerSpec.scala
+++ src/main/scaffolds/form/generated-test/controllers/$model__Camel$ControllerSpec.scala
@@ -12,6 +12,9 @@ import org.scalatestplus.play._
 import play.api.test._
 import play.api.test.Helpers._
 
+import play.filters.csrf.CSRF.Token
+import play.filters.csrf.{CSRFConfigProvider, CSRFFilter}
+
 /**
  * $model;format="Camel"$ form controller specs
  */
