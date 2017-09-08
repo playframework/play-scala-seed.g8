d920324a9f08e105b977958562bf6f097a9bc039
Fabio Pirola <fabio.pirola@databiz.it>
2017-09-08 15:10:18 +0200

    Create a test package in order to avoid name collision/confusion between application classes and test classes.

--- src/main/g8/test/$package$/test/controllers/HomeControllerSpec.scala
+++ src/main/g8/test/$package$/test/controllers/HomeControllerSpec.scala
@@ -1,6 +1,6 @@
-package $package$.controllers
+package $package$.test.controllers
 
-import $package$.controllers
+import $package$.controllers.HomeController
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
